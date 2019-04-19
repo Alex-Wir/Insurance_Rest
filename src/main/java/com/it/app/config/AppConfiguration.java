4bdbb96f21c1c753166850996be467f1cb28abe9
ALL.X <nemanwir@gmail.com>
2019-04-19 20:46:58 +0300

    add config, User and Role

---
+++ src/main/java/com/it/app/config/AppConfiguration.java
@@ -0,0 +1,12 @@
+package com.it.app.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@ComponentScan(basePackages = "com.it")
+@Import({DataBaseConfiguration.class, WebConfiguration.class, MessagesConfiguration.class})
+public class AppConfiguration {
+
+}
