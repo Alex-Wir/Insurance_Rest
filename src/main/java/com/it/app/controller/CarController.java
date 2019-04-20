ddcdfabdb17e54304e2f28b6b1f20e4778a0e745
Alex-Wir <nemanwir@gmail.com>
2019-04-20 21:36:39 +0300

    fix CarController

--- src/main/java/com/it/app/controller/CarController.java
+++ src/main/java/com/it/app/controller/CarController.java
@@ -1,10 +1,8 @@
 package com.it.app.controller;
 
 import com.it.app.component.LocalizedMessageSource;
-import com.it.app.dto.RoleDto;
 import com.it.app.dto.response.CarResponseDto;
 import com.it.app.model.Car;
-import com.it.app.model.Role;
 import com.it.app.service.CarService;
 import org.dozer.Mapper;
 import org.springframework.http.HttpStatus;
@@ -34,12 +32,12 @@ public class CarController {
 
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<CarResponseDto>> getAll() {
-        final List<Car> car = carService.findAll();
-        final List<CarResponseDto> carDtoList = car.stream()
-                .map((role) -> mapper.map(car, CarResponseDto.class))
+        final List<Car> cars = carService.findAll();
+        final List<CarResponseDto> carResponseDtoList = cars.stream()
+                .map((car) -> mapper.map(car, CarResponseDto.class))
                 .collect(Collectors.toList());
-        return new ResponseEntity<>(carDtoList, HttpStatus.OK);
+        return new ResponseEntity<>(carResponseDtoList, HttpStatus.OK);
     }
 
-    //waiting for Car DTO
+    //waiting for other methods
 }
