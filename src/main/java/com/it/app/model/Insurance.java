c563b59fd7de70f7ba274daa716769b7e9cd16f0
Alex-Wir <nemanwir@gmail.com>
2019-04-22 16:37:17 +0300

    add Insurance

---
+++ src/main/java/com/it/app/model/Insurance.java
@@ -0,0 +1,59 @@
+package com.it.app.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+@Entity
+@Table(name = "insurance")
+public class Insurance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "insurance.payment.notNull")
+    @Positive(message = "insurance.payment.positive")
+    private Float payment;
+
+    @NotNull(message = "insurance.amount.notNull")
+    @Positive(message = "insurance.amount.positive")
+    private Float amount;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "car_id", nullable = false)
+    @NotNull(message = "{insurance.car.notNull}")
+    private Car car;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Float getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Float payment) {
+        this.payment = payment;
+    }
+
+    public Float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Float amount) {
+        this.amount = amount;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS user_role (
-id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR(50)
+  id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50)
 );
 
 INSERT INTO user_role (id, name) VALUES (1, 'employee');
@@ -8,22 +8,32 @@ INSERT INTO user_role (id, name) VALUES (2, 'manager');
 INSERT INTO user_role (id, name) VALUES (3, 'customer');
 
 CREATE TABLE IF NOT EXISTS user (
-id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR(50),
-role_id BIGINT,
-FOREIGN KEY (role_id) REFERENCES user_role(id)
+  id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50),
+  role_id BIGINT,
+  FOREIGN KEY (role_id) REFERENCES user_role(id)
 );
 
 INSERT INTO user (id, name, role_id) VALUES (1, 'user.emp', 1);
 INSERT INTO user (id, name, role_id) VALUES (2, 'user.manager', 2);
 INSERT INTO user (id, name, role_id) VALUES (3, 'user.customer', 3);
 
-
 CREATE TABLE IF NOT EXISTS car (
-id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-number VARCHAR(10),
-country VARCHAR(2),
+  id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  number VARCHAR(10),
+  country VARCHAR(2),
 );
 
 INSERT INTO car (id, number, country) VALUES (1, 'AB1234', 'BY');
-INSERT INTO car (id, number, country) VALUES (2, 'ABC987', 'LT')
+INSERT INTO car (id, number, country) VALUES (2, 'ABC987', 'LT');
+
+CREATE TABLE IF NOT EXISTS insurance (
+  id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  payment FLOAT,
+  amount FLOAT,
+  car_id BIGINT,
+  FOREIGN KEY (car_id) REFERENCES car(id)
+);
+
+INSERT INTO insurance (id, payment, amount, car_id) VALUES (1, 12.5, 600, 1);
+INSERT INTO insurance (id, payment, amount, car_id) VALUES (2, 15, 800.45, 2);
