1b2bf2743c28ceb8f81f5d580b7c071be0bd3c3a
Alex-Wir <nemanwir@gmail.com>
2019-04-22 16:09:42 +0300

    fix import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,8 +3,9 @@ id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(50)
 );
 
-INSERT INTO user_role (id, name) VALUES (1, 'user');
-INSERT INTO user_role (id, name) VALUES (2, 'admin');
+INSERT INTO user_role (id, name) VALUES (1, 'employee');
+INSERT INTO user_role (id, name) VALUES (2, 'manager');
+INSERT INTO user_role (id, name) VALUES (3, 'customer');
 
 CREATE TABLE IF NOT EXISTS user (
 id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -13,8 +14,10 @@ role_id BIGINT,
 FOREIGN KEY (role_id) REFERENCES user_role(id)
 );
 
-INSERT INTO user (id, name, role_id) VALUES (1, 'user.simple', 1);
-INSERT INTO user (id, name, role_id) VALUES (2, 'user.admin', 2);
+INSERT INTO user (id, name, role_id) VALUES (1, 'user.emp', 1);
+INSERT INTO user (id, name, role_id) VALUES (2, 'user.manager', 2);
+INSERT INTO user (id, name, role_id) VALUES (3, 'user.customer', 3);
+
 
 CREATE TABLE IF NOT EXISTS car (
 id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
