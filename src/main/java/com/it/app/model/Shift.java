376a527563cdcb3bc811f76ce20f94dab8941cee
Alex-Wir <nemanwir@gmail.com>
2019-05-06 19:28:36 +0300

    ShiftRepository

--- src/main/java/com/it/app/model/Shift.java
+++ src/main/java/com/it/app/model/Shift.java
@@ -15,12 +15,12 @@ public class Shift {
     @NotNull(message = "{shift.opening.notNull")
     private LocalDateTime openingTime;
 
-    //is null when shift is opening
+    //must be Null when shift is opening?
     @NotNull(message = "{shift.closing.notNull}")
     private LocalDateTime closingTime;
 
-    //TODO LAZY
-    @ManyToOne //(fetch = FetchType.LAZY)
+    //TODO NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
--- src/main/java/com/it/app/repository/ShiftRepository.java
+++ src/main/java/com/it/app/repository/ShiftRepository.java
@@ -2,6 +2,17 @@ package com.it.app.repository;
 
 import com.it.app.model.Shift;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ShiftRepository extends JpaRepository<Shift, Long> {
+
+    //TODO!!!!!!!!!!!!!!!!
+    @Query("SELECT s, u FROM Shift s LEFT JOIN FETCH s.user ON user.role=role.name WHERE s.id = ?1")
+    Optional<Shift> findById(Long id);
+
+    @Query("SELECT s FROM Shift s LEFT JOIN FETCH s.user")
+    List<Shift> findAll();
 }
