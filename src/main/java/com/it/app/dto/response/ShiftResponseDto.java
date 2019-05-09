75b7e422d8595f966399396b06bca0711f32d5eb
Alex-Wir <nemanwir@gmail.com>
2019-05-09 21:46:26 +0300

    fix time format in ShiftResponseDto

--- src/main/java/com/it/app/dto/response/ShiftResponseDto.java
+++ src/main/java/com/it/app/dto/response/ShiftResponseDto.java
@@ -1,10 +1,8 @@
 package com.it.app.dto.response;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class ShiftResponseDto {
 
     private Long id;
@@ -13,4 +11,23 @@ public class ShiftResponseDto {
     private UserWithoutRoleResponseDto user;
     private PosResponseDto pos;
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setOpeningTime(String openingTime) {
+        this.openingTime = openingTime.replace('T', ' ');
+    }
+
+    public void setClosingTime(String closingTime) {
+        this.closingTime = closingTime.replace('T', ' ');
+    }
+
+    public void setUser(UserWithoutRoleResponseDto user) {
+        this.user = user;
+    }
+
+    public void setPos(PosResponseDto pos) {
+        this.pos = pos;
+    }
 }
